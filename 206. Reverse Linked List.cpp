71271012a1bedf41776ceecca0fc5d2a81133204
Piyush Kumar <64326511+Dwivedi1823@users.noreply.github.com>
2022-03-27 17:37:12 +0530

    Create 206. Reverse Linked List.cpp
    Runtime: 7 ms, faster than 74.22% of C++ online submissions for Reverse Linked List.
    Memory Usage: 8.3 MB, less than 80.49% of C++ online submissions for Reverse Linked List.

---
+++ 206. Reverse Linked List.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL or head->next == NULL)
+            return head;
+        ListNode *curr = head;
+        ListNode *prev = NULL, *next = NULL;
+        
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr =next;
+        }
+        head = prev;
+        return head;
+    }
+};
